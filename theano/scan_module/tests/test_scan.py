ba3d410ec1a62a9f759dca60d08bd0541080889c
David Warde-Farley <wardefar@iro.umontreal.ca>
2015-05-03 00:29:38 -0400

    Use next() rather than .next() where necessary.

--- theano/scan_module/tests/test_scan.py
+++ theano/scan_module/tests/test_scan.py
@@ -4690,8 +4690,8 @@ def test_speed():
     else:
         while True:
             try:
-                tmp = r_i.next()
-                tmp += r_ii.next()
+                tmp = next(r_i)
+                tmp += next(r_ii)
             except StopIteration:
                 break
     t1 = time.time()
